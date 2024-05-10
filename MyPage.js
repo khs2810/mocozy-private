0055c7bcff50bd776d2bcf9ae93ec9973f1b1344
khs2810 <khs_2810@naver.com>
2024-05-10 17:54:31 +0900

    마이페이지 메뉴바 클릭 호버 완료.

--- MyPage.js
+++ MyPage.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
-  
-    $('#main_menu > li > a').click(function(){
-      $(this).next($('.snd_menu')).slideToggle('fast');
-    })
-    $('.snd_menu > li > a').click(function(e){
-      e.stopPropagation();
-      $(this).next($('.trd_menu')).slideToggle('fast');
-    })
-    
-    
-  })
+  // 메인 메뉴의 클릭 이벤트 처리
+  $('#main_menu > li > a[href=""]').click(function(event){
+      event.preventDefault();
+      // 클릭한 메뉴의 다음에 오는 .snd_menu를 슬라이드 토글
+      $(this).next('.snd_menu').slideToggle('fast');
+  });
+
+  // 서브 메뉴의 클릭 이벤트 처리
+  $('.snd_menu > li > a').click(function(e){
+      e.stopPropagation(); // 부모 요소의 클릭 이벤트 방지
+      // 클릭한 서브 메뉴의 다음에 오는 .trd_menu를 슬라이드 토글
+      $(this).next('.trd_menu').slideToggle('fast');
+  });
+});
+
+
